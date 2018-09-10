2d3902b6762a81543e074cc431cde83787a730a6
Tech-A23 <tech-a23@tech-a23s-air.lan>
2018-09-11 00:46:10 +0100

    initial shop control flow

---
+++ js/app.js
@@ -0,0 +1,111 @@
+console.log('working');
+alert('welcome to the control flow lab, lets go to the shops');
+var item = prompt('what would you like to buy?(fruit/veg/cereal)');
+
+if (item === 'fruit') {
+  var fruit = prompt('we have a variety of fruit,which fruit would you like? (apples/oranges/grapes)');
+  if (fruit==='apples')
+  alert('apples are £2');
+  if (fruit ==='oranges')
+  alert('oranges are £3');
+  if (fruit ==='grapes')
+  alert('grapes are £1');
+}
+
+
+
+else if (item === 'veg'){
+  var veg =prompt('we have a variety of veg, which veg would you like? (carrots/peas/broccoli)');
+  if (veg ==='carrots')
+  alert('carrots are £2');
+  if (veg ==='peas')
+  alert('peas are £3');
+  if (veg ==='broccoli')
+  alert('broccoli is £1');
+
+}
+
+else if (item === 'cereal') {
+  var cereal = prompt('we have a variety of cereal, which cereal would you like (kellogs/cocopops/goldennuggets)')
+  if (cereal==='kellogs')
+  alert('kellogs is £2');
+  if (cereal ==='cocopops')
+  alert('cocopops are £3');
+  if (cereal==='goldennuggets')
+  alert('goldennuggets is £1');
+}
+
+else {
+  alert(`sorry we are sold out of ${item}`)
+}
+
+// if (item === 'fruit') {
+//   var fruit =prompt('we have a variety of fruit,which fruit would you like? (apples/oranges/grapes)');
+
+//   if (fruit === 'apples'); {
+//     alert('apples are £2');
+//
+//   }
+//   if (fruit === 'oranges'); {
+//     alert('oranges are £1');
+//   }
+//     if (fruit ==='grapes'); {
+//     alert('grapes are £3');
+//   }
+//
+//
+//
+// }
+// else if (item === 'veg') {
+//   alert('we have a variety of veg, which veg would you like?');
+//   prompt('peas, carrots or broccoli ');
+//
+//   if (item === 'peas'); {
+//     alert('peas are £2');
+//   }
+//   if (item === 'carrots'); {
+//     alert('carrots are £1');
+//   }
+//    if (item ==='broccoli'); {
+//     alert('broccoli is £3');
+//   }
+//
+//
+// }else if (item === 'cereal') {
+//   alert('we have a variety of cereal, which cereal would you like?');
+//   prompt('cocopops, kellogs or goldennuggets ');
+//
+//   if (item === 'cocopops'); {
+//     alert('cocopops are £2');
+//   }
+//   if (item === 'kellogs'); {
+//     alert('kellogs are £1');
+//   }
+//    if (item ==='grapes'); {
+//     alert('goldennuggets are £3');
+//   }
+// }
+// else {
+//   // alert('sorry we are sold out of' + item)
+//   alert(`sorry we are sold out of ${item}`);
+// }
+
+
+
+
+
+
+console.log(item);
+
+// strings
+// alerts
+// prompts
+// integers/floats
+// operators (+ - * /)
+// booleans
+// array
+// object (bonus)
+// if/else
+// switch
+
+// i go to the shops
